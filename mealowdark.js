899339bab68bc4246d6f642cf5d838b3d3ab98a4
younes <youneskasri@gmail.com>
2018-03-22 15:09:43 -0700

    setting up Handlebars

--- mealowdark.js
+++ mealowdark.js
@@ -2,17 +2,23 @@ var express = require("express");
 
 var app = express();
 
+var handlebars = require("express-handlebars").create({
+	defaultLayout: "main"
+});
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
+
 app.set("port", process.env.PORT || 3000);
 
 
 app.get("/", function(req, res){
-	res.type("text/plain");
-	res.send("Meadowlark Travel");
+	res.render("home");
 });
 
 app.get("/about", function(req, res){
-	res.type("text/plain");
-	res.send("About Meadowlark Travel");
+	res.render("about");
 });
 
 app.get("/about/contact", (req, res)=>res.send(" About Contacts") );
@@ -20,19 +26,16 @@ app.get("/about/contact", (req, res)=>res.send(" About Contacts") );
 app.get("/about*", (req, res)=>res.send(req.route.path));
 
 
-// Custom 404 page
-app.use( function(req, res){
-	res.type("text/plain");
+// 404 catch all handler (middleware)
+app.use( function(req, res, next){
 	res.status(404);
-	res.send("404 - Not Found");
+	res.render("404");
 });
 
-// Custom 500 page
+// 500 error handler (middleware)
 app.use( function(err, req, res, next){
-	console.error(err.stack);
-	res.type("text/plain");
 	res.status(500);
-	res.send("500 - Server Error");
+	res.render("500");
 });
 
 
@@ -40,3 +43,4 @@ app.listen(app.get("port"), function(){
 	console.log("Express started on http://localhost:"
 		+app.get("port")+"; Press Crtl-C to terminate. ");
 });
+
