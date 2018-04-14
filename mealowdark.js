1ba7da920f7def8292914a8f5ddd46cf4dd5deee
younes <youneskasri@gmail.com>
2018-04-14 20:04:21 +0100

    Rendering content and form processing

--- mealowdark.js
+++ mealowdark.js
@@ -53,6 +53,129 @@ app.get("/headers", function(req, res){
 	res.send(s);
 });
 
+// response code other than 200
+app.get("/error", function(req, res){
+	res.status(500).render("error");
+});
+
+// Passing a context to a vue 
+app.get("/greeting2", function(req, res){
+	res.render("about", {
+		message: 'welcome',
+		style: req.query.style,
+		userId: req.cookie.userId,
+		username: req.session.username	
+	});
+});
+
+// Rendering a view without a layout
+app.get("/no-layout", function(req, res){
+	res.render("no-layout", {layout: null});
+});
+
+// Renderig a view with a custom layout
+app.get("/custom-layout", function(req, res){
+	res.render("custom-layout", {layout: "custom"});
+});
+
+// Rendering plain text
+app.get("/test", function(req, res){
+	res.type("text/plain");
+	res.send("this is a test");
+});
+
+
+// Processing Forms
+// bodyParser middleware must be linked in
+app.post("/process-contact", function(req, res){
+	console.log("Retrieved contact from " + req.body.name + '<'+req.body.email+'>');
+	// save to database ...
+	res.redirect(303, "Thank you");
+});
+// a more robust approach
+app.post("/process-contact2", function(req, res){
+	console.log("Retrieved contact from " + req.body.name + '<'+req.body.email+'>');
+	try {
+		// save to database ..
+
+		return res.xhr ? 
+	} catch (ex) {
+		return res.xhr ?
+			res.json({error: "database error"}) :
+			res.redirect("303", "/database-error");
+	}
+});
+
+
+
+// Providing an API
+var tours = [
+	{id: 0, name: "Hood River", price: 99.99},
+	{id: 1, name: "Oregon Coast", price: 149.95}
+];
+// Simple GET endpoint returning only JSON
+app.get("api/tours", function(req ,res){
+	res.json(tours);
+});
+
+app.get("/api/tours", function(req, res){
+	res.json(tours);
+});
+
+
+// GET endpoint that returns JSON, XML, or text
+app.get("/api/tours", function(req, res){
+	var toursXml = '<?xml version="1.0"?><tours>'+
+		products.map(function(p){
+			return '<tour price="'+p.price+'" id="'+p.id+'">' + p.name + '</tour>';
+		}).join('') + '</tours>';
+	var toursText = tours.map(function(p){
+		return p.id+": "+p.name+" ("+p.price+")";
+	}).join('\n');
+
+	res.format({
+		"application/json": function(){
+			res.json(tours);
+		},
+		"text/xml": function(){
+			res.type("text/xml");
+			res.send(toursXml);
+		},
+		"text/plain": function(){
+			res.type("text/plain");
+			res.send(toursXml);
+		}
+	});
+});
+
+
+// PUT endpoint for updating
+app.put("/api/tour/:id", function(req, res){
+	var p = tours.some( function(p){ return p.id == req.params.id });
+	if (p) {
+		if (req.query.name) p.name = req.query.name;
+		if (req.query.price) p.price = req.query.price;
+		res.json({success: true});
+	} else {
+		res.json({error: "No such tour exists"});
+	}
+});
+
+
+// DEL endpoint
+app.del("/api/tour/:id", function(req, res){
+	var i;
+	for(var i=tours.length-1; i>=0; i--)
+		if (tours[i].id == req.params.id) break;
+
+	if (i>=0) {
+		tours.splice(i, 1);
+		res.json({success: true});
+	} else {
+		res.json({error: "No such tour exists"});
+	}
+});
+
 // 404 catch all handler (middleware)
 app.use( function(req, res){
 	res.status(404);
@@ -60,10 +183,17 @@ app.use( function(req, res){
 });
 
 // 500 error handler (middleware)
-app.use( function(err, req, res, next){
+/* this should appear AFTER all of your routes
+note that even if you don't need the "next" function,
+it must be included for Express to recognize this as an
+error handler
+*/
+app.use(function(err, req, res, next){
 	res.status(500);
 	res.render("500");
 });
+
+
 
 
 app.listen(app.get("port"), function(){
